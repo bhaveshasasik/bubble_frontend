c1bd079ffbcb975d1de9745a665b16085a78429a
Bhavesha Sasikumar <bhaveshasasi1234@gmail.com>
2025-02-05 22:56:46 -0800

    Initial Commit

---
+++ bubble/bubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  bubbleApp.swift
+//  bubble
+//
+//  Created by Bhavesh Sasikumar on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct bubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bubbleTests/bubbleTests.swift
@@ -0,0 +1,16 @@
+//
+//  bubbleTests.swift
+//  bubbleTests
+//
+//  Created by Bhavesh Sasikumar on 2/5/25.
+//
+
+import Testing
+
+struct bubbleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
